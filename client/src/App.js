bd9f96fbb81fe3176719e65c194fccd57b65a761
Kevin Peery <kevin.peery@gmail.com>
2018-12-10 08:23:34 -0700

    update App.js and add components files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,67 @@
 import React, { Component } from "react";
-import { Container } from "semantic-ui-react";
+import Menus from "./components/Menus";
+import MenuForm from "./components/MenuForm";
+import { Container, Button, Icon } from "semantic-ui-react";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    menus: [],
+    showForm: false
+  };
+
+  toggleForm = () => this.setState({ showForm: !this.state.showForm });
+
+  componentDidMount() {
+    axios
+      .get("/api/menus")
+      .then(res => {
+        this.setState({ menus: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  addMenu = name => {
+    axios.post("/api/menus", { name }).then(res => {
+      const { menus } = this.state;
+      this.setState({ menus: [...menus, res.data] });
+    });
+  };
+
+  updateMenu = (id, menu) => {
+    axios.put(`/api/menus/${id.id}`, menu).then(res => {
+      const menus = this.state.menus.map(m => {
+        if (m.id === id) return res.data;
+        return m;
+      });
+      this.setState({ menus });
+    });
+  };
+
+  deleteMenu = id => {
+    axios.delete(`/api/menus/${id}`).then(res => {
+      const { menus } = this.state;
+      this.setState({ menus: menus.filter(m => m.id !== id) });
+    });
+  };
+
   render() {
+    const { showForm } = this.state;
     return (
-      <div>
-        Test Here
-      </div>
+      <Container style={{ padding: "30px 0" }}>
+        <Button icon color="blue" onClick={this.toggleForm}>
+          <Icon name={this.state.showForm ? "times" : "plus"} />
+        </Button>
+        New Menu
+        {showForm ? <MenuForm addMenu={this.addMenu} /> : null}
+        <Menus
+          menus={this.state.menus}
+          updateMenu={this.updateMenu}
+          deleteMenu={this.deleteMenu}
+        />
+      </Container>
     );
   }
 }
